c7f701c5286eac74df8ed84a161608cb4edfd21b
Rajasekar V <47376271+Raj252@users.noreply.github.com>
2019-02-06 14:41:22 +0530

    Update oodoreven.java

--- oodoreven.java
+++ oodoreven.java
@@ -7,13 +7,20 @@ class oodoreven
 		int x;
 		Scanner in = new Scanner(System.in);
 		x = in.nextInt();
-		if (x % 2 == 0)
+		if(x>0)
 		{
-			System.out.println("Even");
-		}
+			if (x % 2 == 0)
+			{
+				System.out.println("Even");
+			}
+			else
+			{
+				System.out.println("Odd");
+			}
+			}
 		else
 		{
-			System.out.println("Odd");
+			System.out.println("Invalid");
 		}
    }
 }
