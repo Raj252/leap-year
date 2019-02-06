7ee7faff2f7535d96579bee2974c209c2326de3f
Rajasekar V <47376271+Raj252@users.noreply.github.com>
2019-02-06 15:31:11 +0530

    Create charcount.java

---
+++ charcount.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Scanner; 
+class charcount 
+{
+
+    public static void main(String[] args) 
+	{
+        int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+		
+        char ch;
+        Scanner s= new Scanner(System.in);
+        ch = s.next().charAt(0);
+		
+	
+            if((ch == 'a' || ch == 'e' || ch == 'i'|| ch == 'o' || ch == 'u')||(ch == 'A' || ch == 'E' || ch == 'I'|| ch == 'O' || ch == 'U')) 
+			{
+                ++vowels;
+				System.out.println("Vowels");
+            }
+            else if((ch >= 'a'&& ch <= 'z')||(ch >= 'A'&& ch <= 'Z')) 
+			{
+				++consonants;
+				System.out.println("Consonants");
+            }
+			else
+			{
+				System.out.println("Invalid");
+			}
+    }
+}
