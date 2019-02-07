b10b1244db0c65f23a162ad464094b6a145eef26
Rajasekar V <47376271+Raj252@users.noreply.github.com>
2019-02-07 18:55:32 +0530

    Create yearornot.java

---
+++ yearornot.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class yearornot
+{
+	public static void main(String args[])
+	{
+		int year;
+		Scanner s=new Scanner(System.in);
+		year=s.nextInt();
+		if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+		{
+			System.out.println("Yes");
+		}
+		else
+		{
+			System.out.println("No");
+		}
+	}
+}
