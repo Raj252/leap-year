e12fe47e527d1dddd0df82300c8e004a1042536a
Rajasekar V <47376271+Raj252@users.noreply.github.com>
2019-02-07 07:46:14 +0530

    Create ntimeprint.java

---
+++ ntimeprint.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class ntimeprint
+{
+	public static void main(String args[])
+	{
+		int num,i;
+		Scanner s=new Scanner(System.in);
+		num=s.nextInt();
+		for(i=1;i<=num;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
